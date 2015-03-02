65c015314ef7fa1afef17e5490921940004336b0
dbrio2011 <dbrio2011@gmail.com>
2015-03-02 22:13:35 +0000

    formularios/login.vb listo

---
+++ creditosHipotecarios/formularios/login.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataSetCreditos = New creditosHipotecarios.DataSetCreditos()
+        Me.UsuarioBindingSource = New System.Windows.Forms.BindingSource()
+        Me.UsuarioTableAdapter = New creditosHipotecarios.DataSetCreditosTableAdapters.UsuarioTableAdapter()
+        Me.TableAdapterManager = New creditosHipotecarios.DataSetCreditosTableAdapters.TableAdapterManager()
+        Me.LabelUser = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.LabelPass = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.DataSetCreditos, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.UsuarioBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataSetCreditos
+        '
+        Me.DataSetCreditos.DataSetName = "DataSetCreditos"
+        Me.DataSetCreditos.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'UsuarioBindingSource
+        '
+        Me.UsuarioBindingSource.DataMember = "Usuario"
+        Me.UsuarioBindingSource.DataSource = Me.DataSetCreditos
+        '
+        'UsuarioTableAdapter
+        '
+        Me.UsuarioTableAdapter.ClearBeforeFill = True
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = False
+        Me.TableAdapterManager.UpdateOrder = creditosHipotecarios.DataSetCreditosTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        Me.TableAdapterManager.UsuarioTableAdapter = Me.UsuarioTableAdapter
+        '
+        'LabelUser
+        '
+        Me.LabelUser.AutoSize = True
+        Me.LabelUser.BackColor = System.Drawing.Color.Transparent
+        Me.LabelUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelUser.ForeColor = System.Drawing.Color.White
+        Me.LabelUser.Location = New System.Drawing.Point(80, 163)
+        Me.LabelUser.Name = "LabelUser"
+        Me.LabelUser.Size = New System.Drawing.Size(68, 20)
+        Me.LabelUser.TabIndex = 0
+        Me.LabelUser.Text = "Usuario:"
+        '
+        'txtUser
+        '
+        Me.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUser.Location = New System.Drawing.Point(176, 163)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(100, 22)
+        Me.txtUser.TabIndex = 1
+        '
+        'btnEnter
+        '
+        Me.btnEnter.BackColor = System.Drawing.Color.Transparent
+        Me.btnEnter.BackgroundImage = Global.creditosHipotecarios.My.Resources.Resources.login
+        Me.btnEnter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnEnter.FlatAppearance.BorderColor = System.Drawing.Color.SteelBlue
+        Me.btnEnter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnEnter.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnter.ForeColor = System.Drawing.Color.White
+        Me.btnEnter.Location = New System.Drawing.Point(176, 262)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(100, 26)
+        Me.btnEnter.TabIndex = 3
+        Me.btnEnter.Text = "ENTRAR"
+        Me.btnEnter.UseVisualStyleBackColor = False
+        '
+        'txtPass
+        '
+        Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass.Location = New System.Drawing.Point(176, 210)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(100, 22)
+        Me.txtPass.TabIndex = 2
+        '
+        'LabelPass
+        '
+        Me.LabelPass.AutoSize = True
+        Me.LabelPass.BackColor = System.Drawing.Color.Transparent
+        Me.LabelPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelPass.ForeColor = System.Drawing.Color.White
+        Me.LabelPass.Location = New System.Drawing.Point(52, 210)
+        Me.LabelPass.Name = "LabelPass"
+        Me.LabelPass.Size = New System.Drawing.Size(96, 20)
+        Me.LabelPass.TabIndex = 3
+        Me.LabelPass.Text = "Contraseña:"
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = Global.creditosHipotecarios.My.Resources.Resources.login
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(278, 438)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "SALIR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.creditosHipotecarios.My.Resources.Resources.login
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(360, 475)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.LabelPass)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.LabelUser)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login "
+        CType(Me.DataSetCreditos, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.UsuarioBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataSetCreditos As creditosHipotecarios.DataSetCreditos
+    Friend WithEvents UsuarioBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents UsuarioTableAdapter As creditosHipotecarios.DataSetCreditosTableAdapters.UsuarioTableAdapter
+    Friend WithEvents TableAdapterManager As creditosHipotecarios.DataSetCreditosTableAdapters.TableAdapterManager
+    Friend WithEvents LabelUser As System.Windows.Forms.Label
+    Friend WithEvents txtUser As System.Windows.Forms.TextBox
+    Friend WithEvents btnEnter As System.Windows.Forms.Button
+    Friend WithEvents txtPass As System.Windows.Forms.TextBox
+    Friend WithEvents LabelPass As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ creditosHipotecarios/formularios/login.vb
@@ -0,0 +1,44 @@
+﻿Public Class login
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        'validamos que los campos no estan vacios
+
+        Dim login As Integer = UsuarioTableAdapter.sp_Login(txtUser.Text, txtPass.Text)
+
+        If txtUser.Text = Nothing Then
+            txtUser.Text = MessageBox.Show("Es necesario un usuario", "Campo obligatorio", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtUser.Text = ""
+            txtUser.Focus()
+            txtUser.BackColor = Color.Azure
+
+            Exit Sub
+        End If
+
+
+        If txtPass.Text = Nothing Then
+            txtPass.Text = MessageBox.Show("Es necesario una contraseña", "Campo obligatorio", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtPass.Text = ""
+            txtPass.Focus()
+            txtPass.BackColor = Color.Azure
+
+            Exit Sub
+        End If
+
+        'comprovar si el usuario existe en la base de datos
+
+        If login = 1 Then
+            MsgBox("bien echo campeon")
+        Else
+            MessageBox.Show("Usuario ó Contraseña incorrecta", "Adel Microcréditos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtUser.Text = ""
+            txtPass.Text=""
+            txtUser.Focus()
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+End Class
