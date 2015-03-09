e0e63c6e9972872b2c1f1bdcf503f91eeedbc54c
dbrio2011 <dbrio2011@gmail.com>
2015-03-09 22:41:59 +0000

    casi fin de dia

--- creditosHipotecarios/formularios/creditos.vb
+++ creditosHipotecarios/formularios/creditos.vb
@@ -1,9 +1,38 @@
 ﻿Public Class creditos
     Public clienteCodigo As String
+    Public nombre As String
+
+    Private Sub creditos_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        garantias.Enabled = True
+
+
+    End Sub
+
 
   
     Private Sub creditos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.ClienteTableAdapter.Fill(Me.DataSetAMCProduccion.Cliente, clienteCodigo)
+        Try
+            Me.ClienteTableAdapter.Fill(Me.DataSetAMCProduccion.Cliente, clienteCodigo)
+
+            LabelNombre.Text = nombre
+            With garantias
+                .Enabled = False
+                .GrantiaHipotecariaGridControl.Enabled = False
+
+            End With
+
+        Catch ex As Exception
+            MsgBox("No existe ningun valor")
+            With garantias
+                .Enabled = False
+                .GrantiaHipotecariaGridControl.Enabled = False
+
+            End With
+        End Try
+       
+
 
     End Sub
+
+
 End Class
