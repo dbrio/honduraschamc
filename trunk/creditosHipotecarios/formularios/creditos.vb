f338c90d8141a571139b64d45a630667144e9957
dbrio2011@gmail.com <dbrio2011@gmail.com@2c6d160e-372f-6b59-531b-279f3b85d28e>
2015-03-10 14:17:59 +0000

    empezando el dia

--- trunk/creditosHipotecarios/formularios/creditos.vb
+++ trunk/creditosHipotecarios/formularios/creditos.vb
@@ -3,6 +3,8 @@
     Public nombre As String
     Public propietario As String
     Public descripcion As String
+    Public valorRemate As Decimal
+
 
 
     Private Sub creditos_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
@@ -14,10 +16,20 @@
     Private Function obtenerDatos() As Hashtable
         Dim diccionario As New Hashtable
 
-        diccionario("codigoCliente") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "numclien")
-        diccionario("nombre") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "nomdeudor")
-        diccionario("propietario") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "owngarant")
-        diccionario("descripcion") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "descripcio")
+        diccionario("codigoCliente") = clienteCodigo
+        diccionario("nombre") = nombre
+        diccionario("identidad") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colcnudoci")
+        diccionario("propietario") = propietario
+        diccionario("descripcion") = descripcion
+        diccionario("codigoAgencia") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colccodofi")
+        diccionario("monto") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colnmonsug")
+        diccionario("plazo") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "plazo")
+        diccionario("monto") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colnmonsug")
+        diccionario("intres") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colntasint")
+        diccionario("cuota") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colnmoncuo")
+        diccionario("intresMora") = GridView1.GetRowCellValue(GridView1.FocusedRowHandle, "colnMora_int")
+
+
 
 
         Return diccionario
@@ -55,12 +67,36 @@
 
     Private Sub ClienteGridControl_DoubleClick(sender As Object, e As EventArgs) Handles ClienteGridControl.DoubleClick
         Dim dict As Hashtable = obtenerDatos()
+
         Dim codigoCliente As String = dict("codigoCliente")
         Dim nombre As String = dict("nombre")
         Dim propietario As String = dict("propietario")
+        Dim identidad As String = dict("identidad")
+        Dim codigoAgencia As String = dict("codigoAgencia")
         Dim descripcion As String = dict("descripcion")
+        Dim monto As String = dict("monto")
+        Dim plazo As String = dict("plazo")
+        Dim interes As String = dict("interes")
+        Dim cuota As String = dict("cuota")
+        Dim intesMora As String = dict("interesMora")
+
+       
+
         With addProtocolo
 
+
+            .codigoCliente = codigoCliente
+            .codigoAgencia = codigoAgencia
+            .nombre = nombre
+            .rnp = identidad
+            .propietario = propietario
+            .descripcion = descripcion
+            .monto = monto
+            .plazo = plazo
+            .tasa = interes
+            .cuota = cuota
+            .interesMora = intesMora
+            .valorRemate = valorRemate
             
             .Show()
             .Focus()
