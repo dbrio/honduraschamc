02dd7d358d7be7ed682be2da800f3a6766d5233b
dbrio2011@gmail.com <dbrio2011@gmail.com@2c6d160e-372f-6b59-531b-279f3b85d28e>
2015-03-19 17:25:09 +0000

    filtro codigoAgencia

--- trunk/creditosHipotecarios/formularios/Datos.vb
+++ trunk/creditosHipotecarios/formularios/Datos.vb
@@ -3,9 +3,11 @@
     Public Structure SesionUser
         Public usuario As Integer
         Public cargo As String
+        Public codigoAgencia As String
     End Structure
 
     Public UsuarioActivo As SesionUser
     Public CargoUsuario As SesionUser
+    Public codigoAgencia As SesionUser
 
 End Module
